13fe56c971a450382cdc408c2923b5106fa670a0
sliceoftoni <aneroid_68million@icloud.com>
2025-08-12 14:40:48 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open('tq-v1').then(c => c.addAll(['./','./index.html','./style.css','./app.js','./data/stops.json'])));
+  e.waitUntil(caches.open('tq-v3').then(c => c.addAll(['./','./index.html','./style.css','./app.js','./data/stops.json'])));
 });
 self.addEventListener('fetch', (e) => {
   e.respondWith(caches.match(e.request).then(resp => resp || fetch(e.request)));
